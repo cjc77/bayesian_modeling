6cc8734df62491fb45867c3d578ecd6fee393ba9
Carson <carsonjamescook@gmail.com>
2020-03-26 00:16:30 -0700

    Left off part-way through 'model fitting' section.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="bayesian_modeling",
+    url="https://github.com/cjc77/bayesian_modeling",
+    author="Carson Cook",
+    packages=["util"]
+)
---
+++ util/defs.py
@@ -0,0 +1,2 @@
+from typing import List, Dict, Tuple
+from numpy import ndarray
